88744361852ed271eeff1d7d0628334ecef936ab
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-04-22 10:10:49 -0500

    Parse sample wikipedia page
    Takes a given wikipedia page (hardcoded for now) and parses it into a usable format for deeppavlov.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, request, render_template
 from deeppavlov import build_model, configs
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from unicodedata import normalize
+
 model = build_model(configs.squad.squad, download=True)
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def landing():
     return 'This is the NLP Project 3 API service.'
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    url = 'https://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=Zion%20National%20Park&redirects=true'
+    raw = urlopen(url)
+    soup = BeautifulSoup(raw, 'xml')
+    page = soup.api.query.pages.findAll('page')[0]
+    extract = str(page.extract)
+    text = BeautifulSoup(extract, 'lxml')
+    context = normalize("NFKD", text.get_text())
+    question = 'When were the earliest humans in the park?'
+    response = model([context], [question])
+    return context + '\n\n' + str(response)
 
 
 @app.route('/answer', methods=['GET'])
