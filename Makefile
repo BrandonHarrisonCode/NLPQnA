4ebb98e259630bff8d9edc2196dad2695ba42880
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-04-23 22:48:18 -0500

    Implemented question answering based on location.

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build:
 	docker build -t brandonharrisoncode/nlp-qna .
 
 run: build
-	docker run -it -p 5000:5000 brandonharrisoncode/nlp-qna
+	docker run -it -p 5000:5000 -e GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY} brandonharrisoncode/nlp-qna
--- app.py
+++ app.py
@@ -1,29 +1,60 @@
+import os
+import requests
+import json
+import urllib.parse
 from flask import Flask, request, render_template
 from deeppavlov import build_model, configs
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 from unicodedata import normalize
 
 model = build_model(configs.squad.squad, download=True)
 app = Flask(__name__)
+GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
 @app.route('/', methods=['GET'])
 def landing():
     return 'This is the NLP Project 3 API service.'
 
+def google_results(latitude, longitude):
+    print('API key: {}'.format(GOOGLE_MAPS_API_KEY))
+    url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?location={},{}&query=national%20parks&type=park&key={}'.format(latitude, longitude, GOOGLE_MAPS_API_KEY)
+    print('URL: {}'.format(url))
+    result = requests.get(url)
+    if result.ok:
+        print('Response ok')
+        return json.loads(result.content)
+    return None
+
 
 @app.route('/test', methods=['GET'])
 def test():
-    url = 'https://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=Zion%20National%20Park&redirects=true'
-    raw = urlopen(url)
+    question = 'How big is the park?'
+    latitude = '47.6918452'
+    longitude = '-122.2226413'
+    maps_results = google_results(latitude, longitude)
+    locations = maps_results['results']
+    national_parks = []
+    for location in locations:
+        if 'National Park' in location['name']:
+            national_parks.append(location['name'])
+
+    responses = []
+    for park in national_parks[:6]:
+        page = get_wikipedia_page(park)
+        responses.append((park, model([page], [question])))
+    return str(responses)
+
+
+def get_wikipedia_page(title):
+    url = 'https://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles={}&redirects=true'.format(title)
+    raw = requests.get(url).content
     soup = BeautifulSoup(raw, 'xml')
     page = soup.api.query.pages.findAll('page')[0]
     extract = str(page.extract)
     text = BeautifulSoup(extract, 'lxml')
-    context = normalize("NFKD", text.get_text())
-    question = 'When were the earliest humans in the park?'
-    response = model([context], [question])
-    return context + '\n\n' + str(response)
+    page = normalize("NFKD", text.get_text())
+    return page
+
 
 
 @app.route('/answer', methods=['GET'])
